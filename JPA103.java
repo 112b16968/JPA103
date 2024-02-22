88945d21d2548278a14ad7a1f81494543c923956
112b16968 <160818741+112b16968@users.noreply.github.com>
2024-02-22 14:12:35 +0800

    JPA103.java

--- JPA103.java
+++ JPA103.java
@@ -6,15 +6,19 @@ public class JPA103 {
         Scanner a =  new Scanner(System.in);
         //讀取整行
         String b = a.nextLine();
+        //分割空格並且放入陣列裡面
         String[] nums = b.split("\\s+");
         double total = 0;
         for (int i=0;i<nums.length;i++){
+            //將字串轉成浮點數
             double numbers = Double.parseDouble(nums[i]);
             total += numbers;
         }
         double average = total/nums.length;
         System.out.printf("Average: %.2f%n",average);
 
+    
     }
     
 }
+
